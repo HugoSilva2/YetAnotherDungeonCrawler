c785cbb0de9434c58d4ae45f633984e86cc9b162
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-18 21:33:21 +0100

    Added the main gameplay loop as a switch case without the interactions

--- YetAnotherDungeonCrawler/Controller.cs
+++ YetAnotherDungeonCrawler/Controller.cs
@@ -7,6 +7,10 @@ namespace YetAnotherDungeonCrawler
 {
     public class Controller
     {
+         private Player player;
+        private List<Room> rooms;
+        private int currentRoomIndex;
+        private bool gameRunning = false;
         public void Start(IView view)
         {
             view.Intro();
@@ -18,6 +22,54 @@ namespace YetAnotherDungeonCrawler
                 new Room(2, new Enemy("Jannitor", 80, 15), new Item("Broom")),
                 new Room(3, new Enemy("Novice", 200, 30), new Item("Glasses"))
             };
+
+            gameRunning = true;
+            while (gameRunning)
+            {
+                Room currentRoom = rooms[currentRoomIndex];
+                view.DisplayRoomInfo(currentRoom);
+                string action = view.GetPlayerInput().ToLower();
+
+                switch (action)
+                {
+                    case "move":
+                        
+                        
+                        break;
+
+                    case "attack":
+                        player.Attack(currentRoom.Enemy);
+                        if (currentRoom.Enemy.Health <= 0)
+                        {
+                            
+                        }
+                        else
+                        {
+                            
+                        }
+                        break;
+
+                    case "pickup":
+                        if (currentRoom.Item != null)
+                        {
+                            player.PickupItem(currentRoom.Item);
+                            currentRoom.Item = null;  
+                        }
+                        else
+                        {
+                        
+                        }
+                        break;
+
+                    case "quit":
+                        gameRunning = false;
+                        break;
+
+                    default:
+                        
+                        break;
+                }
+            }
             
 
         }
