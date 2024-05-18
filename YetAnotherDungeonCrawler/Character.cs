6e9ee40c06040412f892701514cdf5947eefb026
Leonardo Henrique <leonardojose@netcabo.pt>
2024-05-18 17:56:10 +0100

    created Character.cs and added Attack method

---
+++ YetAnotherDungeonCrawler/Character.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetAnotherDungeonCrawler
+{
+    public class Character
+    {
+        string Name { get; set; }
+        int Health { get; set; }
+        int MaxHealth { get; set; }
+        int AttackPower { get; set; }
+        List<Item> Inventory { get; set; }
+    }
+
+    Character(int MaxHealth, int AttackPower,)
+    {
+        MaxHealth = MaxHealth;
+        MaxHealth = MaxHealth;
+        AttackPower = AttackPower;
+        Inventory = new List<Item>();
+    }
+    
+    void Attack(Enemy enemy)
+    {
+        Console.WriteLine($"{Name} attacked {enemy.Name} for {AttackPower} damage.");
+        enemy.Health -= AttackPower;
+        if (enemy.Health <= 0)
+        {
+            Console.WriteLine($"{enemy.Name} was slain.")
+            enemy.Health = 0;
+        }
+        else
+        {
+            Console.WriteLine($"{enemy.Name} has {enemy.Health} health remaining");
+        }
+    }
+}
