165cf3f8cbdb7854be1aea4872ae97e6d69579a5
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-19 17:12:20 +0100

    Just added more methods from View

--- YetAnotherDungeonCrawler/View.cs
+++ YetAnotherDungeonCrawler/View.cs
@@ -8,17 +8,16 @@ namespace YetAnotherDungeonCrawler
         public void Intro()
         {
             Console.WriteLine("Welcome to Yet Another Dungeon Crawler!");
-            Console.WriteLine("So here's the Rules:");
-            Console.WriteLine("You must travel through this dungeon to find the long lost artifact");
-            Console.WriteLine("There will be enemies and items in some rooms");
-            Console.WriteLine("You can attack, move, pickup items and use items");
-            Console.WriteLine("If you enter a room with an enemy you must kill the enemy before leaving");
+            Console.WriteLine("You must travel through this dungeon to find the long lost artifact.");
+            Console.WriteLine("There will be enemies and items in some rooms.");
+            Console.WriteLine("You can attack, move, pick up items and use items.");
+            Console.WriteLine("If you enter a room with an enemy, you must kill the enemy before leaving.");
             Console.WriteLine("The game ends when you die or when you retrieve the lost artifact!");
         }
 
         public void Move(string name, int id)
         {
-            // Implementation of Move
+            Console.WriteLine($"{name} moves to room {id}.");
         }
 
         public void Attack(string name1, string name2, int damage)
@@ -31,25 +30,35 @@ namespace YetAnotherDungeonCrawler
             Console.WriteLine($"{name} healed {heal} HP.");
         }
 
-        public void RoomDescription()
+        public void RoomDescription(Room room)
         {
-            // Implementation of RoomDescription
+            Console.WriteLine($"Room ID: {room.Id}");
+            Console.WriteLine($"Enemy: {room.Enemy.Name}");
+            Console.WriteLine("Items in the room:");
+            foreach (var item in room.Items)
+            {
+                Console.WriteLine($"- {item.Name}");
+            }
         }
 
         public void PickUpItem(string name, Item item)
         {
-            Console.WriteLine($"{name} picked up {item.Name}");
+            Console.WriteLine($"{name} picked up {item.Name}.");
         }
 
         public void UI(int health, int maxhealth, int attackpower)
         {
-            Console.Write($"Health: {health}/{maxhealth}");
-            Console.WriteLine($"  Attack Power: {attackpower}");
+            Console.WriteLine($"Health: {health}/{maxhealth}");
+            Console.WriteLine($"Attack Power: {attackpower}");
         }
-        
-        public void Inventory()
+
+        public void Inventory(List<Item> inventory)
         {
-            // Implementation of Inventory
+            Console.WriteLine("Inventory:");
+            foreach (var item in inventory)
+            {
+                Console.WriteLine($"- {item.Name}");
+            }
         }
 
         public void TheEnd()
@@ -59,26 +68,15 @@ namespace YetAnotherDungeonCrawler
             Console.WriteLine("THE END");
         }
 
-        public void DisplayRoomInfo(Enemy enemy, List<Item> items)
+        public string GetPlayerInput()
         {
-            Console.WriteLine($"Enemy: {enemy.Name}");
-            Console.WriteLine("Items in the room: ");
-            foreach (var item in items)
-            {
-                Console.WriteLine(item.Name);
-            }
+            Console.Write("Enter action: ");
+            return Console.ReadLine();
         }
-    }
 
-    
-
-    public class Item
-    {
-        public string Name { get; set; }
-    }
-
-    public class Enemy
-    {
-        public string Name { get; set; }
+        public void DisplayRoomInfo(Room room)
+        {
+            RoomDescription(room);
+        }
     }
 }
