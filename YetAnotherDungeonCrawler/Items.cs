fa9886dd5fcb2b4e9b69805679f6db6640702f23
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-19 17:08:43 +0100

    Added a small Items template just so the code can run for debuging

---
+++ YetAnotherDungeonCrawler/Items.cs
@@ -0,0 +1,12 @@
+namespace YetAnotherDungeonCrawler
+{
+    public class Items
+    {
+        public string Name { get; set; }
+
+        public Items(string name)
+        {
+            Name = name;
+        }
+    }
+}
