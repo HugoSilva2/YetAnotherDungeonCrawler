9ac35a6d829772ce0368eacf8bacdcf579d12c53
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-05-19 19:36:02 +0100

    Finalized the Move aspect of the game

--- YetAnotherDungeonCrawler/Controller.cs
+++ YetAnotherDungeonCrawler/Controller.cs
@@ -7,10 +7,8 @@ namespace YetAnotherDungeonCrawler
     {
         private Player player;
         private List<Room> rooms;
-        private int currentRoomIndex;
+        private int currentRoomId;
         private bool gameRunning = false;
-
-        private int currentRoomId = 0;
 
         public void Start(IView view)
         {
@@ -19,15 +17,21 @@ namespace YetAnotherDungeonCrawler
             player = new Player("Lord W Rizz", 100, 20);
             rooms = new List<Room>
             {
-                new Room(0, new Enemy("Loyal Intern", 50, 10), new List<Item> { new Item("Coffee Mug") }),
-                new Room(7, new Enemy("Janitor", 80, 15), new List<Item> { new Item("Broom") }),
-                new Room(2, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") })
+                new Room(-4, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") }),
+                new Room(-3, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") }),
+                new Room(-2, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") }),
+                new Room(-1, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") }),
+                new Room(0, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") }),
+                new Room(1, new Enemy("Loyal Intern", 50, 10), new List<Item> { new Item("Coffee Mug") }),
+                new Room(2, new Enemy("Janitor", 80, 15), new List<Item> { new Item("Broom") }),
+                new Room(3, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") }),
+                new Room(4, new Enemy("Novice", 200, 30), new List<Item> { new Item("Glasses") })
             };
 
             gameRunning = true;
             while (gameRunning)
             {
-                Room currentRoom = rooms[currentRoomIndex];
+                Room currentRoom = rooms.Find(r => r.Id == currentRoomId);
                 view.DisplayRoomInfo(currentRoom);
                 view.ListActions();
                 string action = view.GetPlayerInput().ToLower();
@@ -35,31 +39,29 @@ namespace YetAnotherDungeonCrawler
                 switch (action)
                 {
                     case "move right":
-                        currentRoomIndex += 2;
-                        if (currentRoomIndex >= rooms.Count)
+                        currentRoomId += 1;
+                        if (!rooms.Exists(r => r.Id == currentRoomId))
                         {
+                            currentRoomId -= 1;
                             Console.WriteLine("Can't move any further");
-                            break;                       
                         }
-                        view.Move(player.Name, currentRoomIndex);
-
+                        else
+                        {
+                            view.Move(player.Name, currentRoomId);
+                        }
                         break;
+
                     case "move left":
-                        currentRoomIndex -= 2;
-                        view.Move(player.Name, currentRoomIndex);
-
-                        break;
-
-                    case "move up":
-                        currentRoomIndex += 7;
-                        view.Move(player.Name, currentRoomIndex);
-
-                        break;
-
-                    case "move down":
-                        currentRoomIndex -= 7;
-                        view.Move(player.Name, currentRoomIndex);
-
+                        currentRoomId -= 1;
+                        if (!rooms.Exists(r => r.Id == currentRoomId))
+                        {
+                            currentRoomId += 1;
+                            Console.WriteLine("Can't move any further");
+                        }
+                        else
+                        {
+                            view.Move(player.Name, currentRoomId);
+                        }
                         break;
 
                     case "attack":
